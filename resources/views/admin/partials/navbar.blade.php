1fed021ec99482d8c8d1f595045dfb69bcf3553c
RoberHuang <roberhuang@outlook.com>
2018-12-23 14:25:36 +0800

    后台模板和导航

---
+++ resources/views/admin/partials/navbar.blade.php
@@ -0,0 +1,45 @@
+<ul class="nav navbar-nav">
+    <li class="nav-item"><a class="nav-link" href="/">首页</a></li>
+    @auth('admin')
+        <li @if (Request::is('admin/post*')) class="nav-item active" @else class="nav-item" @endif>
+            <a class="nav-link" href="/admin/post">文章</a>
+        </li>
+        <li @if (Request::is('admin/tag*')) class="nav-item active" @else class="nav-item" @endif>
+            <a class="nav-link" href="/admin/tag">标签</a>
+        </li>
+        <li @if (Request::is('admin/upload*')) class="nav-item active" @else class="nav-item" @endif>
+            <a class="nav-link" href="/admin/upload">上传</a>
+        </li>
+    @endauth
+</ul>
+
+<!-- Right Side Of Navbar -->
+<ul class="nav navbar-nav navbar-right">
+    <!-- Authentication Links -->
+    {{--@guest--}}
+    @if (auth()->guard('admin')->guest())
+        <li><a href="{{ route('admin.login') }}">Login</a></li>
+        {{--<li><a href="{{ route('admin.register') }}">Register</a></li>--}}
+    @else
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                {{ Auth::guard('admin')->user()->name }} <span class="caret"></span>
+            </a>
+
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="{{ route('admin.logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+            </ul>
+        </li>
+    @endif
+    {{--@endguest--}}
+</ul>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,75 +8,39 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('blog.title', 'blog') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @yield('styles')
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+{{-- Navigation Bar--}}
+<nav class="navbar navbar-expand-md navbar-light nav-laravel">
+    <div class="container">
+        <a class="navbar-brand mr-auto mr-lg-0" href="#">{{ config('blog.title', 'blog') }} 后台</a>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+        <!-- Collapsed Hamburger -->
+        <button type="button" class="navbar-toggle p-0 border-0" type="button" data-toggle="collapse" data-target="#app-navbar-collapse"
+                aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        {{--@guest--}}
-                        @if (auth()->guard('admin')->guest())
-                            <li><a href="{{ route('admin.login') }}">Login</a></li>
-                            {{--<li><a href="{{ route('admin.register') }}">Register</a></li>--}}
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::guard('admin')->user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('admin.logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                        {{--@endguest--}}
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            @include('admin.partials.navbar')
+        </div>
     </div>
+</nav>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+<main class="py-4">
+    @yield('content')
+</main>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+
+@yield('scripts')
 </body>
 </html>
