8b7319dda6b96fff89ea3ed89b3d86411ddd3a17
RoberHuang <roberhuang@outlook.com>
2019-02-03 14:38:59 +0800

    路由修改

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,11 @@ Route::post('/payments/notify', 'PaymentsController@notify');    // ping++后台
 
 Route::prefix('admin')->group(function ($router) {
     $router->redirect('/', '/admin/posts', 301);
-    $router->resource('/posts', 'PostsController');
+    $router->resource('/posts', 'PostsController', ['except' => 'show']);
     $router->resource('/categories', 'CategoriesController', ['except' => 'show']);
     $router->resource('/tags', 'TagsController', ['except' => 'show']);
 
-    $router->post('/configs/setOrder', 'ConfigController@setOrder');
+    $router->post('/configs/setOrder', 'ConfigsController@setOrder');
     $router->post('/configs/setConf', 'ConfigsController@setConf');
     $router->resource('/configs', 'ConfigsController', ['except' => 'show']);
 
@@ -45,15 +45,15 @@ Route::prefix('admin')->group(function ($router) {
 });
 
 Route::prefix('admin')->namespace('Admin')->group(function ($router) {
-    $router->get('login', 'LoginController@showLoginForm')->name('admin.login');
-    $router->post('login', 'LoginController@login');
-    /*$router->get('register', 'LoginController@showRegister')->name('admin.register');
-    $router->post('register', 'LoginController@register');*/
-    $router->post('logout', 'LoginController@logout')->name('admin.logout');
-    $router->post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');       //发送重置密码的邮件
-    $router->get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');     //发送重置密码邮件的表单页面
-    $router->post('password/reset', 'ResetPasswordController@reset');                                                   //重置密码
-    $router->get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('admin.password.reset');      //重置密码表单
+    $router->get('/login', 'LoginController@showLoginForm')->name('admin.login');
+    $router->post('/login', 'LoginController@login');
+    /*$router->get('/register', 'LoginController@showRegister')->name('admin.register');
+    $router->post('/register', 'LoginController@register');*/
+    $router->post('/logout', 'LoginController@logout')->name('admin.logout');
+    $router->post('/password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');       //发送重置密码的邮件
+    $router->get('/password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');     //发送重置密码邮件的表单页面
+    $router->post('/password/reset', 'ResetPasswordController@reset');                                                   //重置密码
+    $router->get('/password/reset/{token}', 'ResetPasswordController@showResetForm')->name('admin.password.reset');      //重置密码表单
 
-    $router->get('task/{project}', 'TaskController@index');
+    $router->get('/task/{project}', 'TaskController@index');
 });
